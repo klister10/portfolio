5142f8b344599be41e74215cee386ec2cd713005
Kathleen Lister <kathleen@amiralearning.com>
2024-04-24 11:07:08 -0400

    hello world plus favicon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<svg className="leaves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+        <g>
+          <animateTransform attributeName="transform" type="rotate" from="0 50 50" to="360 50 50" dur="10s" repeatCount="indefinite" />
+          <path d="M50,0C22.4,0,0,22.4,0,50c0,27.6,22.4,50,50,50s50-22.4,50-50C100,22.4,77.6,0,50,0z M50,95C24.5,95,5,75.5,5,50 C5,24.5,24.5,5,50,5s45,19.5,45,45C95,75.5,75.5,95,50,95z" fill="#8BC34A" />
+        </g>
+      </svg>*/}
+      Hello World
     </div>
   );
 }
+
 
 export default App;
