67870fc331ad6bb73e930f7ae4a03b07395f2dc4
Kathleen Lister <kathleen@amiralearning.com>
2024-04-25 08:59:44 -0400

    adding public url prefix

--- src/App.js
+++ src/App.js
@@ -9,36 +9,36 @@ import React, { useEffect, useState } from 'react';
 const portfolioItems = [
   {
     title: "Amira Learning",
-    videoURL: "/assets/amira_demo.mov",
+    videoURL: process.env.PUBLIC_URL + "/assets/amira_demo.mov",
     linkURL: "https://www.amiralearning.com/",
     description: "An AI based Reading Tutor. I worked with this ed-tech startup as Front-End Tech Lead.",
   },
   {
     title: "Wander Match",
-    videoURL: "/assets/wander_match_demo.mp4",
+    videoURL: process.env.PUBLIC_URL + "/assets/wander_match_demo.mp4",
     description: "A flight destination discovery app using the skyscanner and google places apis.",
   },
   {
     title: "Tax Calculator",
-    videoURL: "/assets/tax_calculator_demo.mov",
+    videoURL: process.env.PUBLIC_URL + "/assets/tax_calculator_demo.mov",
     description: "A tool to determine tax withholding for people with variable income.",
     linkURL: "https://master.d2fk9tf53c80qb.amplifyapp.com/",
   },
   {
     title: "TripAdvisor",
-    imgURL: "/assets/TA_screen_shot.png",
+    imgURL: process.env.PUBLIC_URL + "/assets/TA_screen_shot.png",
     linkURL: "https://www.tripadvisor.com/CheapFlightsHome",
     description: "I owned the flights mobile web which was written in ReactJS.",
   },
   {
     title: "Behind the Scenes",
-    imgURL: "/assets/github_logo.png",
+    imgURL: process.env.PUBLIC_URL + "/assets/github_logo.png",
     linkURL: "https://github.com/klister10",
     description: "Visit my GitHub.",
   },
   {
     title: "Let's Connect",
-    imgURL: "/assets/linkedin_logo.png",
+    imgURL: process.env.PUBLIC_URL + "/assets/linkedin_logo.png",
     linkURL: "https://www.linkedin.com/in/kathleen-lister-perlman/",
     description: "Connect with me on LinkedIn.",
   },
